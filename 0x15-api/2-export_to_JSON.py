147c1f82cfd785d946073d5a63bd560961723c8d
robertokoba7 <okobarobert7@gmail.com>
2023-01-16 18:45:44 +0300

    [add] export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,23 +3,23 @@
 return information about his/her TODO list progress
 and exports data to json
 """
- 
+
 import requests
 from sys import argv
 import json
- 
- 
+
+
 BASE_URL = 'https://jsonplaceholder.typicode.com'
- 
+
 user = requests.get(f'{BASE_URL}/users/{argv[1]}').json()
 todos = requests.get(f'{BASE_URL}/todos', params={"userId": argv[1]}).json()
- 
+
 with open(f'{argv[1]}.json', 'w', encoding='utf8') as f:
     tasks = []
     for todo in todos:
         tasks.append({"task": f'{todo["title"]}',
                       "completed": f'{str(todo["completed"])}',
                       "username": f'{user["name"]}'})
-    data = {f"{argv[1]}": tasks}	
+    data = {f"{argv[1]}": tasks}
     json_string = json.dumps(data)
     f.write(json_string.rstrip())
