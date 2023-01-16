2ff11a08b86075c14a742cede7e145d950cdbbda
Robert Okoba <104755875+robertokoba7@users.noreply.github.com>
2023-01-16 16:54:16 +0300

    Update README.d

--- 0x15-api/README.d
+++ 0x15-api/README.d
@@ -1,1 +1,6 @@
 # API
+Project done during Full Stack Software Engineering studies at ALX SEProgramme and Holberton School. It aims to learn about how to build a regular expression.
+
+## Technologies
+*Scripts written in python3
+*Tested on Ubuntu 20.04 LTS:wq
