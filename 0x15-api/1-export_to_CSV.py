d2fc8c293311735a467668622d0b45363f73708f
robertokoba7 <okobarobert7@gmail.com>
2023-01-16 18:42:34 +0300

    [add] export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,8 +5,8 @@ return information about his/her TODO list progress"""
 from csv import writer, QUOTE_ALL
 from requests import get
 from sys import argv
- 
- 
+
+
 def todo_csv(emp_id):
     """ Send request for employee's
         to do list to API
@@ -14,10 +14,10 @@ def todo_csv(emp_id):
     file_name = '{}.csv'.format(emp_id)
     url_user = 'https://jsonplaceholder.typicode.com/users/'
     url_todo = 'https://jsonplaceholder.typicode.com/todos/'
- 
+
     # check if user exists
     user = get(url_user + emp_id).json().get('username')
- 
+
     if user:
         params = {'userId': emp_id}
         #  get all tasks
@@ -29,9 +29,9 @@ def todo_csv(emp_id):
                 task_writer = writer(f, quoting=QUOTE_ALL)
                 for task in tasks:
                     task_writer.writerow([emp_id, user, task.get('completed'),
-                                         task.get('title')])
- 
- 
+                                          task.get('title')])
+
+
 if __name__ == '__main__':
     if len(argv) > 1:
         todo_csv(argv[1])
