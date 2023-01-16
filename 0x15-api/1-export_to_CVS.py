d7331cec9e1a49f67fea9475aa3113c656c6cf19
robertokoba7 <okobarobert7@gmail.com>
2023-01-16 18:32:35 +0300

    [add] export to CVS

--- 0x15-api/1-export_to_CVS.py
+++ 0x15-api/1-export_to_CVS.py
@@ -7,31 +7,26 @@ from requests import get
 from sys import argv
  
  
-def todo_csv(emp_id):
-    """ Send request for employee's
-        to do list to API
-    """
-    file_name = '{}.csv'.format(emp_id)
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
- 
-    # check if user exists
-    user = get(url_user + emp_id).json().get('username')
- 
-    if user:
-        params = {'userId': emp_id}
-        #  get all tasks
-        tasks = get(url_todo, params=params).json()
-        if tasks:
-            #  open file in write mode and use csv writer to
-            #  writer content
-            with open(file_name, 'w', newline='', encoding='utf8') as f:
-                task_writer = writer(f, quoting=QUOTE_ALL)
-                for task in tasks:
-                    task_writer.writerow([emp_id, user, task.get('completed'),
-                                         task.get('title')])
- 
- 
-if __name__ == '__main__':
-    if len(argv) > 1:
-        todo_csv(argv[1])
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = r.json()
+    for user in users:
+        if user.get('id') == int(argv[1]):
+            user_name = user.get('username')
+            user_id = user.get('id')
+            break
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = r.json()
+    csv_file = str(user_id)+".csv"
+
+    with open(csv_file, mode='w', encoding="UTF8") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            if todo.get('userId') == user_id:
+                writer.writerow([
+                    user_id,
+                    user_name,
+                    todo.get('completed'),
+                    todo.get('title')
+                    ])
