3b963e0455c22cae82c2b44102e898c05fa7e9c1
robertokoba7 <okobarobert7@gmail.com>
2023-01-31 19:01:40 +0300

    [add]: Fetch top ten posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,6 +3,7 @@
 Requests module for sending HTTP requests to the Reddit API
 """
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
@@ -11,9 +12,9 @@ def top_ten(subreddit):
 
     headers = {'User-Agent': 'MyRedditBot/0.0.1'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        posts = response.json()['data']['children']
+    request = requests.get(url, headers=headers, allow_redirects=False)
+    if (request.status_code == 200):
+        posts = request.json()['data']['children']
         for post in posts:
             print(post['data']['title'])
     else:
